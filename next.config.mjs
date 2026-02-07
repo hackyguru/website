86a5a21702b5eac5afc11063b39970eeef25883e
Kumaraguru <19eucs071@skcet.ac.in>
2026-02-07 01:10:03 +0000

    fixing blog image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'blog.codex.storage',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blog.waku.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'opengraph.b-cdn.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
